ff01d9e84cf0503b1b2557aebed96dbde2de2ad4
lasconic <lasconic@gmail.com>
2017-04-13 09:02:32 +0200

    change model for a more mozart one, bind 0.0.0.0

--- server/server.py
+++ server/server.py
@@ -1,18 +1,18 @@
-# 
+#
 # Copyright 2016 Google Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 
 from predict import generate_midi
 import os
@@ -37,7 +37,7 @@ def predict():
     midi_data = pretty_midi.PrettyMIDI(StringIO(''.join(chr(v) for v in values)))
     duration = float(request.args.get('duration'))
     ret_midi = generate_midi(midi_data, duration)
-    return send_file(ret_midi, attachment_filename='return.mid', 
+    return send_file(ret_midi, attachment_filename='return.mid',
         mimetype='audio/midi', as_attachment=True)
 
 
@@ -47,4 +47,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='0.0.0.0', port=8080)
